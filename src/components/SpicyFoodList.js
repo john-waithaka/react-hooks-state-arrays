d1e3df39dca04c38c2eb6b639444dfb87c858cfb
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-06-03 00:26:00 +0300

    working with arrays

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,32 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+
+    // Add new food by using the spread operator to make a copy of our foods array and insert each element into a new array.
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+    
+    console.log(newFood); // Log the new food when it's added
   }
 
+  // Handle the click on a food item to increase its heat level
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+
+  // Create a list of food items
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
